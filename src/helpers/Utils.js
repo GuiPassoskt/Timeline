1df4fa9de1f7b7451a28ce0024c264f541eee50a
Guilherme Moreira <guipassoskt@gmail.com>
2020-01-16 00:19:44 -0300

    Atualizando funções

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -1,35 +1,43 @@
+import moment from 'moment'
+
 const Utils = {
 
     OrderByDesc (data, name) {
-        return data.sort(function(a,b) { 
-            return new Date(b[name]).getTime() - new Date(a[name]).getTime() 
+        return data.sort((a,b) => { 
+          return new Date(a[name]).getTime() - new Date(b[name]).getTime() 
         })
     },
 
     GetHourTime (hour) {
-        return new Date(hour).getHours() +':'+ new Date(hour).getMinutes()
+      return new Date(hour).getHours() +':'+ new Date(hour).getMinutes()
     },
 
     GetDate (date) {
-        return new Date(date).getUTCDate() +'/'+ new Date(date).getMonth() +'/'+ new Date(date).getFullYear()
+      return moment(date).format('DD/MM/YYYY')
     },
 
     FormatJSON: events => events.map((item) => {
-        const event = {
-          event: item.event,
-          timestamp: item.timestamp
-        }
-    
-        if (item.revenue) {
-          event.revenue = item.revenue;
-        }
-    
-        for (let i = 0; i < item.custom_data.length; i++) {
-          event[item.custom_data[i].key] = item.custom_data[i].value;
-        }
-    
-        return event;
-    })
+      const event = {
+        event: item.event,
+        timestamp: item.timestamp
+      }
+  
+      if (item.revenue) {
+        event.revenue = item.revenue;
+      }
+  
+      for (let i = 0; i < item.custom_data.length; i++) {
+        event[item.custom_data[i].key] = item.custom_data[i].value;
+      }
+  
+      return event;
+    }),
+
+    filters: {
+      FormatCurrency: (money) => {
+        return 'R$ ' + money.toFixed(2).replace('.',',')
+      }
+    }
 
 }
 
