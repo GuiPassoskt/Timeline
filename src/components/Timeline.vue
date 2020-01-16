46f4940a53a089eee219e29ab2750414a185d90c
Guilherme Moreira <guipassoskt@gmail.com>
2020-01-16 00:53:12 -0300

    Separando o estilo do componente

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -66,28 +66,6 @@ export default {
 }
 </script>
 
-<style scoped>
-.timeline {
-  position: relative;
-  max-width: 1200px;
-  margin: 0 auto;
-}
-
-.timeline::after {
-  content: '';
-  position: absolute;
-  width: 6px;
-  background-color: #dbdfe5;
-  top: 0;
-  bottom: 0;
-  left: 50%;
-  margin-left: -3px;
-}
-
-@media screen and (max-width: 600px) {
-  .timeline::after {
-    left: 31px;
-  }
-  
-}
+<style lang="scss" scoped>
+  @import '../assets/scss/Timeline.scss';
 </style>
